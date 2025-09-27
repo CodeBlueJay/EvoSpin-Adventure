9e3feca42a18df808893402aa489898936159aa1
CodeBlueJay <codebluejay12@gmail.com>
2025-09-26 23:58:06 -0700

    added sum spin logic

--- EvoSpin/Form1.cs
+++ EvoSpin/Form1.cs
@@ -8,30 +8,39 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace EvoSpin
-{
-    public partial class Form1 : Form
-    {
+namespace EvoSpin {
+    public partial class Form1 : Form {
         string item = "";
         string[] items = { "Dog", "Ball", "Honda Civic", "Cosmic Leviathan" };
-        public Form1()
-        {
+        double[] weights = { 0.33, 0.33, 0.33, 0.01 };
+        public Form1() {
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
-            lblroll.Text = $"You got:\n{item}";
+        private void Form1_Load(object sender, EventArgs e) {
+            lblroll.Text = $"You got: {item}";
         }
 
-        private void Lblroll_Click(object sender, EventArgs e)
-        {
-            lblroll.Text = "test";
+        private void Lblroll_Click(object sender, EventArgs e) {
         }
 
-        private void Roll_Click(object sender, EventArgs e)
-        {
-            lblroll.Text = "hi";
+        private void Roll_Click(object sender, EventArgs e) {
+            double totalWeight = weights.Sum();
+            double randomValue = new Random().NextDouble() * totalWeight;
+            double cumulative = 0.0;
+            int selectedIndex = 0;
+            for (int i = 0; i < weights.Length; i++)
+            {
+                cumulative += weights[i];
+                if (randomValue < cumulative)
+                {
+                    selectedIndex = i;
+                    break;
+                }
+            }
+            item = items[selectedIndex];
+            int chance = (int)(1 / weights[selectedIndex]);
+            lblroll.Text = $"You got: {item} (1 in {chance})";
         }
     }
 }
